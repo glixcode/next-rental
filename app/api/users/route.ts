1630dc6fdafe4ab5a5bb05d937d5308e28064add
glixcode <glix.coder@gmail.com>
2026-05-26 07:31:22 +0800

    added a test get user endpoint

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -3,6 +3,10 @@ import {userLoginSchema} from './schema'
 import { NextResponse } from 'next/server'
 import bcrypt from 'bcrypt'
 
+export async function GET(request: Request){
+    return NextResponse.json({name: "John Doe"})
+}
+
 export async function POST(request: Request){
     const body = await request.json()
     const username:string = body.username as string
